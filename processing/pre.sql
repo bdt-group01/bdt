14f8fa0d3d06848eb621d72a31f3d8c63525e7ce
qingdannn <qingdannn@gmail.com>
2020-12-08 15:14:56 +0800

    Update pre.sql

--- processing/pre.sql
+++ processing/pre.sql
@@ -1,15 +1,17 @@
 -- duplication reduction
-DELETE FROM userbehavior WHERE (user,item,category,behavior,time)
+DELETE FROM userbehavior 
+WHERE (user,item,category,behavior,time)
 IN 
 (SELECT user,item,category,behavior,time 
-	FROM (SELECT user,item,category,behavior,time 
-				FROM userbehavior 
-				GROUP BY user,item,category,behavior,time 
-				HAVING COUNT(*)>1) s1) 
-AND id NOT IN (SELECT id FROM 
-							(SELECT id FROM userbehavior 
-												 GROUP BY user,item,category,behavior,time 
-												 HAVING COUNT(*)>1) s2);
+	FROM (SELECT user,item,category,behavior,time
+		  FROM userbehavior
+		  GROUP BY user,item,category,behavior,time
+		  HAVING COUNT(*)>1) s1) 
+AND id NOT IN 
+(SELECT id FROM 
+	(SELECT id FROM userbehavior 
+		GROUP BY user,item,category,behavior,time 
+		HAVING COUNT(*)>1) s2);
 -- missing valuse reduction
 SELECT count(user),count(item),count(category),count(behavior),count(time) FROM userbehavior;
 
